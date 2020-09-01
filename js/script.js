313b5c7c89dc4bcd3624f1e988befe020791644a
itulupov1 <itulupov1@yandex.ru>
2020-09-02 00:00:35 +0700

    lesson04-complicated

--- js/script.js
+++ js/script.js
@@ -1,51 +1,15 @@
 'use strict';
 
-const lang = 'en'; // По условию 'ru' или 'en'
-const ruWeek = ['пн, вт, ср, чт, пт, сб, вс'];
-const enWeek = ['m, ts, w, th, f, st, sn'];
+const strFunc = function(arg) {
+	if (typeof (arg) !== 'string'){
+		console.log('В качестве аргумента используется не строка!');
+		return 0;
+	}
+	arg = arg.trim();
+	console.log(
+		arg.length > 30 ? arg.slice(0, 30) + '...' : arg
+	);
 
-//Вариант a, через if
+};
 
-if (lang === 'en') {
-	console.log(enWeek);
-} else if (lang === 'ru') {
-	console.log(ruWeek);
-} else {
-	console.log('Что-то пошло не так');
-}
-
-//Вариант b, через switch-case
-switch (lang) {
-	case 'en':
-		console.log(enWeek);
-		break;
-	case 'ru':
-		console.log(ruWeek);
-		break;
-	default:
-		console.log('Что-то пошло не так');
-		break;
-}
-
-//Вариант c, через массив
-const arr = [
-	['m, ts, w, th, f, st, sn'],
-	['пн, вт, ср, чт, пт, сб, вс'],
-];
-
-if (lang === 'en') {
-	console.log(arr[0]);
-} else if (lang === 'ru') {
-	console.log(arr[1]);
-} else {
-	console.log('Что-то пошло не так');
-}
-
-
-//Тернарный оператор
-const namePerson = 'Артем';
-console.log(
-	(namePerson === 'Артем') ? 'Директор' :
-		(namePerson === 'Максим') ? 'Преподователь' : 'Студент'
-);
-
+strFunc('В полученной(как аргумент) строке функция должна убрать все пробелы в начале и в конце');
