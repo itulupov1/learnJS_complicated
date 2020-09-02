0c5bccca4d446028be24b1bb1981c9eff6c5134a
itulupov1 <itulupov1@yandex.ru>
2020-09-02 13:29:18 +0700

    lesson05-complicated

--- js/script.js
+++ js/script.js
@@ -1,15 +1,34 @@
 'use strict';
 
-const strFunc = function(arg) {
-	if (typeof (arg) !== 'string'){
-		console.log('В качестве аргумента используется не строка!');
-		return 0;
+// lesson05-complicated
+
+const arr = [];
+
+arr.push('2574', '16582', '7896', '47614', '761258', '45762', '789621');
+
+for (let i = 0; i < arr.length; i++) {
+	if (arr[i].substr(0, 1) === '2') {
+		console.log(arr[i]);
+	} else if (arr[i].substr(0, 1) === '4') {
+		console.log(arr[i]);
 	}
-	arg = arg.trim();
-	console.log(
-		arg.length > 30 ? arg.slice(0, 30) + '...' : arg
-	);
+}
 
-};
 
-strFunc('В полученной(как аргумент) строке функция должна убрать все пробелы в начале и в конце');
+function isPrime(num) {
+	let start = 2;
+	const limit = Math.sqrt(num);
+	while (start <= limit) {
+		if (num % start++ < 1) {
+			return false;
+		}
+	}
+	return num > 1;
+}
+
+for (let i = 2; i < 100; i++) {
+	if (isPrime(i) === true) {
+		console.log(`Делители этого числа: 1 и ${i}`);
+	}
+}
+
